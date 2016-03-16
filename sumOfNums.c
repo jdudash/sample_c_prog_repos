20e2b4909d0c883e196771f9dfc3af0f9cbbf42c
Jason R Dudash <jdudash43@hotmail.com>
2016-03-16 18:26:39 +0000

    added some printf commands

--- sumOfNums.c
+++ sumOfNums.c
@@ -11,5 +11,8 @@ int main(int argc, const char * argv[]) {
     
     printf("The sum of the numbers from 1 to 10 is %d.", sum);
     
+    printf("");
+    printf("It appears that my attempt has worked.");
+    
     return 0;
 }
