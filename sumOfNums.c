66e32399ee58d71ba6d685739254942da21d93db
Jason R Dudash <jdudash43@hotmail.com>
2016-03-16 18:13:47 +0000

    Created new program.

---
+++ sumOfNums.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int number, sum;
+    
+    sum = 0;
+    
+    for(number=1; number<=10; number++) {
+        sum += number;
+    }
+    
+    printf("The sum of the numbers from 1 to 10 is %d.", sum);
+    
+    return 0;
+}
